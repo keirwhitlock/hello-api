317345bc0ac9a9af2a383c1456d1ab240da6c669
Keir Whitlock <keir.whitlock@groundcontrol.com>
2024-05-26 12:01:23 +0100

    chapter 3.6

--- handlers/rest/translate_test.go
+++ handlers/rest/translate_test.go
@@ -50,10 +50,7 @@ func TestTranslateAPI(t *testing.T) {
 		}
 
 		var resp Resp
-		err := json.Unmarshal(rr.Body.Bytes(), &resp)
-		if err != nil {
-			panic(err)
-		}
+		json.Unmarshal(rr.Body.Bytes(), &resp)
 
 		if resp.Language != test.ExpectedLanguage {
 			t.Errorf(`expected language to be "%s" but received "%s"`, test.ExpectedLanguage, resp.Language)
